0899958c336e7bd666040645bdf25fd9989d3dd6
bobur-ops <bobur0172@gmail.com>
2023-01-31 22:46:27 +0200

    fix tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: netlify()
 });
